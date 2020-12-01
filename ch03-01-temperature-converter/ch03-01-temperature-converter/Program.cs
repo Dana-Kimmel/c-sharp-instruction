2985904875d067ae07a38412be030adb7f27a4eb
Dana-Kimmel <dear.prudence34@gmail.com>
2020-12-01 14:35:11 -0500

    init commit

---
+++ ch03-01-temperature-converter/ch03-01-temperature-converter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ch03_01_temperature_converter {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Temperature Converter!");
+            
+            String choice = "y";
+            while(choice.Equals("y")) {
+                Console.WriteLine("Enter degrees in Fahrenheit: ");
+                double degreeFahrenheit = Double.Parse(Console.ReadLine());
+
+                double degreeCelsius = (degreeFahrenheit - 32) * 5 / 9;
+                double degreeCelsiusRounded = Math.Round(degreeCelsius, 2);
+
+                Console.WriteLine("Degrees in Celsius: " + degreeCelsiusRounded);
+
+                Console.WriteLine("Continue? (y/n)");
+                choice = Console.ReadLine();
+                
+            }
+            Console.WriteLine("Bye now :)");
+        }
+    }
+}
