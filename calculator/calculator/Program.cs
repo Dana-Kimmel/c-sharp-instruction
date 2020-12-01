3c58b67567f010422f6f6ade12fdda8b988a4d8d
Dana-Kimmel <dear.prudence34@gmail.com>
2020-12-01 13:54:55 -0500

    init commit

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace calculator {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Area and Perimeter Calculator!");
+
+            String choice = "y";
+            while (choice.Equals("y")) {
+
+                Console.WriteLine("Enter length: ");
+                int length = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter width: ");
+                int width = Int32.Parse(Console.ReadLine());
+
+                int area = width * length;
+                int perimeter = (2 * width) + (2 * length);
+
+                Console.WriteLine("Area: " + area + "\n" + "Perimeter: " + perimeter);
+
+                Console.WriteLine("Continue? (y/n)");
+                choice = Console.ReadLine();
+
+                Console.WriteLine("Bye now :)");
+            }
+             
+        }
+    }
+}
