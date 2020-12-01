3f431fa6dea316d9a33b4e5fe065f727db282b92
Dana-Kimmel <dear.prudence34@gmail.com>
2020-12-01 14:58:28 -0500

    init commit

---
+++ ch03-02-travel-time-calculator/ch03-02-travel-time-calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ch03_02_travel_time_calculator {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Travel Time Calculator!");
+
+            String choice = "y";
+            while(choice.Equals("y")) {
+
+                Console.WriteLine("Enter miles: ");
+                double miles = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter miles per hour: ");
+                double mph = Double.Parse(Console.ReadLine());
+
+                int hours = (int) ((int) miles / mph);
+                int min = (int)(((miles / mph) * 60) % 60);
+
+                Console.WriteLine("Estimated travel time: ");
+                Console.WriteLine("---------------------");
+                Console.WriteLine("Hours: " + hours);
+                Console.WriteLine("Minutes: " + min);
+
+                Console.WriteLine("Continue? (y/n) ");
+                choice = Console.ReadLine();
+
+                
+            }
+            Console.WriteLine("Bye now :)");
+        }
+    }
+}
