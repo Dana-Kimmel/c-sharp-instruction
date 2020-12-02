1da1f7f46936bef2f404185e28bc9a9e19750607
Dana-Kimmel <dear.prudence34@gmail.com>
2020-12-02 15:35:55 -0500

    init commit

---
+++ ch03-03-interest-calculator/ch03-03-interest-calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ch03_03_interest_calculator {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Interest Calculator!");
+            String choice = "y";
+            while (choice.Equals("y")) {
+                Console.WriteLine("Enter loan amount: ");
+                double loanAmount = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter interest rate: ");
+                double interestRate = Double.Parse(Console.ReadLine());
+                double interest = loanAmount * interestRate;
+                double interestRounded = Math.Round(interest, 2);
+                Console.WriteLine("Loan amount: " + loanAmount.ToString("C"));
+                Console.WriteLine("Interest rate: " + interestRate.ToString("P"));
+                Console.WriteLine("Interest: " + interestRounded.ToString("C"));
+                Console.WriteLine("Continue? (y/n)");
+                choice = Console.ReadLine();
+            }
+            Console.WriteLine("Bye now :)");
+        }
+    }
+}
+    
