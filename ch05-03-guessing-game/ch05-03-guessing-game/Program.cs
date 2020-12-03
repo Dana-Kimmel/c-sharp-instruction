f75eeec1541e6d5b83777dae39bf199096ea4b24
Dana-Kimmel <dear.prudence34@gmail.com>
2020-12-03 15:22:53 -0500

    added GetInt method

---
+++ ch05-03-guessing-game/ch05-03-guessing-game/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ch05_03_guessing_game {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
