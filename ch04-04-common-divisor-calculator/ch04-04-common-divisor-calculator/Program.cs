239f5c15fc91089fed654f733f7abeecb6dcb785
Dana-Kimmel <dear.prudence34@gmail.com>
2020-12-03 14:03:11 -0500

    init commit

--- ch04-04-common-divisor-calculator/ch04-04-common-divisor-calculator/Program.cs
+++ ch04-04-common-divisor-calculator/ch04-04-common-divisor-calculator/Program.cs
@@ -4,7 +4,31 @@ namespace ch04_04_common_divisor_calculator {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Greatest Common Divisor Finder!");
+            String choice = "y";
+            while (choice.Equals("y") || (choice.Equals("Y"))) {
+                Console.WriteLine("Enter first number: ");
+                int firstNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second number: ");
+                int secondNumber = int.Parse(Console.ReadLine());
 
+                int gCD = CalculateGreatestCommonDivisor(firstNumber, secondNumber);
+                Console.WriteLine("Greatest common divisor " + gCD);
+
+                Console.WriteLine("Continue? (y/n)");
+                choice = Console.ReadLine();
+            }
+            Console.WriteLine("Bye now :)");
+        }
+        public static int CalculateGreatestCommonDivisor(int x, int y) {
+            while (x != 0) {
+                while (y >= x) {
+                    y = y - x;
+                }
+                int tmp = x;
+                x = y;
+                y = tmp;
+            }
+            return y;
         }
     }
 }
