77fe70f57326d63e20d0a24f57a0583bd2a41e60
Dana-Kimmel <dear.prudence34@gmail.com>
2020-12-04 17:06:00 -0500

    added GetInt and while loop

--- ch05-03-guessing-game/ch05-03-guessing-game/Program.cs
+++ ch05-03-guessing-game/ch05-03-guessing-game/Program.cs
@@ -3,7 +3,48 @@
 namespace ch05_03_guessing_game {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine("Welcome to the Guess the Number Game!");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++");
+			var r = new Random();
+			int numToGuess = r.Next(100);
+
+			int userGuess = 0;
+			int numGuesses = 0;
+			while (userGuess != numToGuess) {
+				numGuesses++;
+				userGuess = GetInt("Enter a number between 1 and 100: ", 1, 100);
+				Console.WriteLine("Try to guess it");
+				if (userGuess > (10 + numToGuess)) {
+					Console.WriteLine("Way to high!");
+				} else if (userGuess > numToGuess) {
+					Console.WriteLine("Too high!");
+				} else if (userGuess < (numToGuess - 10)) {
+					Console.WriteLine("Way too low!");
+				} else if (userGuess < numToGuess) {
+					Console.WriteLine("Too low!");
+				} else {
+					Console.WriteLine("You got it in " + numGuesses + " tries!");
+				}
+
+			}
+		}
+		public static int GetInt(String prompt, int min, int max) {
+			int n = 0;
+			Boolean success = false;
+			while (!success) {
+				Console.Write(prompt);
+				n = int.Parse(Console.ReadLine());
+				if (n <= min) {
+					Console.WriteLine("Error! Number must be greater than " + min + ".");
+				} else if (n >= max) {
+					Console.WriteLine("Error! Number must be less than " + max + ".");
+				} else {
+					success = true;
+				}
+			}
+			return n;
+		}
+
+		}
+	}
+
